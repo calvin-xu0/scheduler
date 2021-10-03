ed90e53e0065d8fcbacf3439a75d24aa1e7d9d58
Calvin Xu <calvin.xu.jy.@gmail.com>
2021-10-03 02:25:53 -0400

    Implement initial transitions in appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,31 @@
 import React from "react";
 import "./index.scss";
+import { useVisualMode } from "hooks/useVisualMode";
 import Header from "./header";
 import Show from "./show";
 import Empty from "./empty";
+import Form from "./Form";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
+
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show {...props.interview}/> : <Empty />}
+      {/* {props.interview ? <Show {...props.interview}/> : <Empty />} */}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => transition(EMPTY)} />}
     </article>
   );
 };
