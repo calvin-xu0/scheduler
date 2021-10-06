824ba94dd081b0b790187086b41bbbf9df51e48a
Calvin Xu <calvin.xu.jy.@gmail.com>
2021-10-06 19:44:39 -0400

    Implement integration tests for appointments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -22,13 +22,28 @@ describe("Appointments", () => {
   it("should edit an interview", () => {
     cy.get("[alt=Edit]")
       .first()
-      .click({force=true});
+      .click({force: true});
     
-    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-    cy.get("[alt='Sylvia Palmer']").click();
+    cy.get("[data-testid=student-name-input]").clear().type("John Smith");
+    cy.get("[alt='Tori Malcolm']").click();
+
     cy.contains("Save").click();
 
-    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-    cy.contains(".appointment__card--show", "Sylvia Palmer");
+    cy.contains(".appointment__card--show", "John Smith");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .first()
+      .click({force: true});
+
+    cy.contains("Confirm").click();
+
+    cy.contains(/Deleting/i).should("exist");
+    cy.contains(/Deleting/i).should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
   });
 })
