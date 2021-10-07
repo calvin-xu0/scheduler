7a7a133beae45bf132b03f445475312598412494
Calvin Xu <calvin.xu.jy.@gmail.com>
2021-10-07 00:16:17 -0400

    Improve readability and documentation

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Interview Scheduler
 
+*Interactive Scheduler* is a single page application built using React that allows users to book an appointment with an interviewer.
+
+Data is persisted by an API server using a PostgreSQL database. This client application communicates with an API server over HTTP, using the JSON format.
+
+Jest tests are used through the development of the project.
+
 ## Setup
 
 Install dependencies with `npm install`.
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,7 +5,8 @@ export const useVisualMode = initialMode => {
   const [history, setHistory] = useState([initialMode]);
 
   const transition = (newMode, replace = false) => {
-    return setMode(newMode), replace ? setHistory(prev => [newMode, ...prev.slice(1)]) : setHistory(prev => [newMode, ...prev]);
+    setMode(newMode);
+    replace ? setHistory(prev => [newMode, ...prev.slice(1)]) : setHistory(prev => [newMode, ...prev]);
   };
   const back = () => {
     return history.length > 1 ? (setHistory(prev => [...prev.slice(1)]), setMode(history[1])) : setMode(prev => prev);
