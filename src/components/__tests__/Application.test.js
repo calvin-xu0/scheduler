31035d5e60cc7ae21b9fcec638cb9796351b432a
Calvin Xu <calvin.xu.jy@gmail.com>
2021-11-11 23:03:05 -0500

    Implement integration testing with container scope

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,15 +1,50 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+describe("Application", () => {
 
-  await waitForElement(() => getByText("Monday"));
-  fireEvent.click(getByText("Tuesday"));
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
+  it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+
+    await waitForElement(() => getByText("Monday"));
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+
+  it("loads data, books an interview, and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    
+    const appointment = getAllByTestId(container, "appointment")[0];
+    // const { getByText, getAllByAltText, getByPlaceholderText, getAllByRole } = render(<Application />);
+
+    fireEvent.click(getByAltText(appointment, "Add"));
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    
+    fireEvent.click(getByText(appointment, "Save"));
+    // const addButtonElement = getAllByAltText("Add")[0];
+    // fireEvent.click(addButtonElement);
+
+    // fireEvent.change(getByPlaceholderText("Enter Student Name"), {
+    //   target: { value: "Lydia Miller-Jones" }
+    // });
+
+    // const interviewerElement = getAllByRole("listitem")[0];
+    // fireEvent.click(interviewerElement);
+
+    // fireEvent.click(getByText("Save"));
+
+    // await waitForElement(() => getByText("Saving..."));
+    console.log(prettyDOM(container));
+  })
+
+})
